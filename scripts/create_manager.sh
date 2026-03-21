8948902da1914e2d9b833910380c92bffadd08ad
Mikalay <miko@mikodeb.mikodeb.com>
2026-03-21 17:33:40 +0100

    additional checks for manager script and adaptation for split approach

--- scripts/create_manager.sh
+++ scripts/create_manager.sh
@@ -9,67 +9,26 @@ fi
 # prevent interacive prompting for apt stuff
 export DEBIAN_FRONTEND=noninteractive
 
-declare KUBE_VERSION="1.35"
+# cluster address range 
 declare CLUSTER_CID="192.168.123.0/16"
 
 
+# Check if initial setup script has been done and finished correctly
+# Verify the binary exists
+if ! command -v kubelet &> /dev/null; then
+    echo "Kubelet binary not found!"
+    echo "R u sure intial setup was executed?"
+    exit 1
+fi
 
-# switch swap off and distable it in fstab
-swapoff -a 
-sed -i '/^[^#]/s/^\(\S\+\s\+\S\+\s\+\)swap/\1swap/; /^[^#].*\s\+swap\s\+/s/^/# /' /etc/fstab
-
-
-#tee overwrites the things anyway so no point checkng
-mkdir -p /etc/modules-load.d
-tee /etc/modules-load.d/containerd.conf <<EOF
-overlay
-br_netfilter
-EOF
-
-# load added kernel modules
-modprobe overlay
-modprobe br_netfilter
-
-#modify kubernetes conf file
-tee /etc/sysctl.d/kubernetes.conf <<EOT
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOT
-sysctl --system
-
-
-# install and dearmor support packages for docker
-apt update
-apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
-# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-apt install -y containerd
-
-mkdir -p /etc/containerd
-containerd config default > /etc/containerd/config.toml
-
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-sed -i 's/disabled_plugins = \["cri"\]//' /etc/containerd/config.toml
-
-systemctl restart containerd
-systemctl enable containerd
-
-#sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-#systemctl restart containerd
-#systemctl enable containerd
-
-
-# get install kubernetes based on the version provided or default
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBE_VERSION/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBE_VERSION/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
-apt update
-apt install -y kubelet kubeadm kubectl
-
-systemctl enable kubelet # it's rare for it not to enable automatically but better safe then troubleshoot
+# Verify the service is enabled (will start on next boot/trigger)
+if systemctl is-enabled --quiet kubelet; then
+    echo "Base components ready for kubeadm."
+else
+    echo "Service not enabled."
+    echo "Check setup! It may have failed!"
+    exit 1
+fi
 
 # make sure not to run this thing 2 times if stuff exists already
 if [ ! -f /etc/kubernetes/admin.conf ]; then
@@ -94,7 +53,7 @@ KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f \
 https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/calico.yaml
 
 # lock versions after all the stuff is installed in order for the package updates not to break up the cluster
-apt-mark hold kubelet kubeadm kubectl
+# apt-mark hold kubelet kubeadm kubectl
 
 echo "Waiting for control plane pods..."
 
