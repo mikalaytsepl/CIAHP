088b0b0f9d1f29614918563d9eff8fd31e148fdd
mikalaytsepl <mikalaytsepl@gmail.com>
2026-03-29 10:05:11 +0200

    inventory manager basic functionality, starting playbooks testing

--- scripts/integration_scripts/inventory_manager.py
+++ scripts/integration_scripts/inventory_manager.py
@@ -11,22 +11,18 @@ class HostType(str,Enum):
     WORKER="worker"
 
 class InventoryManager():
-    def __init__(self, path_to_inventory:str):
+    def __init__(self, path_to_inventory: str):
         self.inv_path_ = Path(path_to_inventory)
-        
-        # if path exists, do nothing. if not, mkdir -p basically
-        self.inv_path_.parent.mkdir(parents=True,exist_ok=True)
-
-        # same but for file at the end 
+        self.inv_path_.parent.mkdir(parents=True, exist_ok=True)
         self.inv_path_.touch(exist_ok=True)
 
-        # if file is empty, initialize the structure: 
         if self.inv_path_.stat().st_size == 0:
             self._save({
                 "all": {
                     "children": {
-                        "managers": {"hosts": {}},
-                        "workers": {"hosts": {}}
+                        "global_managers": {"children": {}},
+                        "global_workers": {"children": {}},
+                        "clusters": {"children": {}}
                     }
                 }
             })
@@ -54,7 +50,7 @@ class InventoryManager():
         # check if ssh port is open 
         try: 
             result = sub.run(
-                ["nc", "-z", "-w", str(sub), ip, "22"],
+                ["nc", "-z", "-w", str(3), ip, "22"],
                 capture_output=True,
                 text=True,
             )
@@ -68,77 +64,95 @@ class InventoryManager():
         return True
 
     def _load(self):
-        with open(self.inv_path_, 'r') as file:
-            return yaml.safe_load(file)
-    
+            with open(self.inv_path_, 'r') as file:
+                return yaml.safe_load(file) or {}
+
     def _save(self, data):
         with open(self.inv_path_, 'w') as file:
-            return yaml.safe_dump(data, file, sort_keys=False)
-        
-    def _get_group(self, inv, group):
-        return inv["all"]["children"].setdefault(group, {"hosts": {}})["hosts"]
+            # sort keys false is to keep yaml human readable
+            yaml.safe_dump(data, file, sort_keys=False, indent=4)
+
     # CRUD logic 
 
-    def add_host(self, name: str, ip: str, group: Literal["managers", "workers"]):
+    def add_host(self, name: str, ip: str, cluster_name: str, role: Literal["managers", "workers"]):
             inv = self._load()
+            
+            # ensure global role exists 
+            global_role_group = f"global_{role}"
+            all_children = inv["all"]["children"]
+            
+            # setup chierarchy
+            cluster_root = all_children["clusters"]["children"].setdefault(cluster_name, {"children": {}})
+            role_group_name = f"{cluster_name}_{role}"
+            
+            # add specific rule group to the cluster
+            cluster_root["children"].setdefault(role_group_name, {"hosts": {}})
+            
+            # link role group to the global group
+            all_children[global_role_group]["children"].setdefault(role_group_name, {})
 
-            hosts = self._get_group(inv, group)
-
-            if name in hosts:
-                raise ValueError(f"Host '{name}' already exists in {group}")
-
-            hosts[name] = {"ansible_host": ip}
+            # Add the actual host data
+            host_list = cluster_root["children"][role_group_name]["hosts"]
+            if name in host_list:
+                print(f"Warning: Host {name} already exists in {role_group_name}. Updating IP.")
+            
+            host_list[name] = {"ansible_host": ip}
 
             self._save(inv)
+            print(f"Successfully added {name} ({ip}) to {cluster_name} as a {role[:-1]}.")
 
-    def delete_host(self, name: str):
+    def delete_host(self, name: str, cluster_name: str, role: Literal["managers", "workers"]):
+            inv = self._load()
+            role_group = f"{cluster_name}_{role}"
+            
+            try:
+                hosts = inv["all"]["children"]["clusters"]["children"][cluster_name]["children"][role_group]["hosts"]
+                if name in hosts:
+                    del hosts[name]
+                    self._save(inv)
+                    print(f"Deleted {name} from {cluster_name}")
+                    return
+            except KeyError:
+                pass
+
+            raise ValueError(f"Host '{name}' not found in {cluster_name} {role}")
+
+    # get all hosts 
+    def _find_host_location(self, inv, name):
+        """Returns (cluster, role) if host exists, else (None, None)"""
+        clusters = inv["all"]["children"]["clusters"]["children"]
+        for c_name, c_data in clusters.items():
+            for r_group, r_data in c_data["children"].items():
+                if name in r_data.get("hosts", {}):
+                    # derive role from a group name
+                    role = "managers" if "managers" in r_group else "workers"
+                    return c_name, role
+        return None, None
+    
+    # gets full inventory and cluster name as arguments
+    def get_cluster_resouces(self, inv, cluster_name: str) -> dict:
+        try:
+            # just scopes to children of a specific clsuter (the main goal is to do that IN PLACE)
+            return inv["all"]["children"]["clusters"]["children"][cluster_name]["children"]
+        except KeyError:
+            raise ValueError(f"Cluster '{cluster_name}' not found")
+        
+    def truncate_cluster_resources(self,cluster_name:str) -> None:
         inv = self._load()
+        cluster_resources = self.get_cluster_resouces(inv,cluster_name)
+        
+        # clear out resources of managers and workers hosts
+        cluster_resource_groups = (f"{cluster_name}_managers",f"{cluster_name}_workers")
+        for resoruce_group in cluster_resource_groups:
+            cluster_resources[resoruce_group]['hosts'].clear()
+        self._save(inv)
 
-        for group in ["managers", "workers"]:
-            hosts = self._get_group(inv, group)
-            if name in hosts:
-                del hosts[name]
-                self._save(inv)
-                return
-
-        raise ValueError(f"Host '{name}' not found")
-
-    def update_host(
-        self,
-        name: str,
-        new_name: str | None = None,
-        new_ip: str | None = None,
-        new_group: Literal["managers", "workers"] | None = None
-    ):
-        inv = self._load()
-
-        # Find host
-        for group in ["managers", "workers"]:
-            hosts = self._get_group(inv, group)
-
-            if name in hosts:
-                host_data = hosts[name]
-
-                # Remove old entry
-                del hosts[name]
-
-                # Apply updates
-                updated_name = new_name or name
-                updated_ip = new_ip or host_data["ansible_host"]
-                target_group = new_group or group
-
-                target_hosts = self._get_group(inv, target_group)
-
-                if updated_name in target_hosts:
-                    raise ValueError(f"Host '{updated_name}' already exists in {target_group}")
-
-                target_hosts[updated_name] = {"ansible_host": updated_ip}
-
-                self._save(inv)
-                return
-
-        raise ValueError(f"Host '{name}' not found")
     
 
-inventory = InventoryManager(path_to_inventory="/home/miko/CIAHP/ansible/inventory.yml")
-inventory.delete_host("manager_test_1")
+if __name__ == '__main__':
+    inventory = InventoryManager('/home/miko/CIAHP/ansible/inventory.yml')
+
+    inventory.truncate_cluster_resources("my-cluster")
+
+    inventory.add_host("new-worker","172.16.86.131","my-cluster","workers")
+    inventory.add_host("new-manager","172.16.86.130","my-cluster","managers")
