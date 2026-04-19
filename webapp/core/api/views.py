f24595fcfc3bf1e32961292cf12fd76df6624f18
tomada16 <tomadam1@poczta.onet.pl>
2026-04-19 21:01:08 +0200

    Added simple html to templates and updated urls.py and views.py
    Templates work - for now acces by only changing url

--- webapp/core/api/views.py
+++ webapp/core/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+	return render(request, 'login.html')
+
+def dashboard(request):
+	return render(request, 'dashboard.html')
+
+def instances(request):
+	return render(request, 'instances.html')
+
+def hardening(request):
+	return render(request, 'hardening.html')
--- webapp/core/core/urls.py
+++ webapp/core/core/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from api.api import api  # importing api file with ninja logic 
+from api.api import api  # importing api file with ninja logic
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),   # binding ninja routes so they will be under /api/
+	path('instances/', views.instances, name='instances'),
+	path('hardening/', views.hardening, name='hardening'),
+	path('login/', views.login_view, name='login'),
+	path('', views.dashboard, name='dashboard'),
 ]
