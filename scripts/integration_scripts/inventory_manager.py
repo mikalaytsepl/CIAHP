29c9a49aadf6cfb584cb97f8e2ddb54f81fd9b79
mikalaytsepl <mikalaytsepl@gmail.com>
2026-05-11 22:22:42 +0200

    more hardening and that's hopefully it

--- scripts/integration_scripts/inventory_manager.py
+++ scripts/integration_scripts/inventory_manager.py
@@ -175,11 +175,11 @@ class InventoryManager():
 
 if __name__ == '__main__':
     test = InventoryManager('/home/miko/CIAHP/ansible/inventory.yml')
-    test.add_host("main_manager","172.16.86.153","hardeningfwtest","managers")
-    test.add_host("second_manager","172.16.86.154","hardeningfwtest","managers")
-    test.add_host("first_worker","172.16.86.155","hardeningfwtest","workers")
-    test.add_host("first_worker","172.16.86.156","hardeningfwtest","workers")
+    test.add_host("main_manager","172.16.86.161","hardeningfwtest","managers")
+    test.add_host("second_manager","172.16.86.162","hardeningfwtest","managers")
+    test.add_host("first_worker","172.16.86.163","hardeningfwtest","workers")
+    test.add_host("second_worker","172.16.86.164","hardeningfwtest","workers")
     test.set_cluster_ha_vars(
         cluster_name= "hardeningfwtest",
-        vip_address="172.16.86.177"
+        vip_address="172.16.86.185"
     )
